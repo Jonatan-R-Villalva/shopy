17dabe981aa212442992b09ba340bc5fedddf8b3
= <=>
2023-06-10 01:15:20 -0300

    renderizando informacion de productos en componente productDetail

--- vite-project/src/Components/ProductDetail/index.jsx
+++ vite-project/src/Components/ProductDetail/index.jsx
@@ -12,10 +12,21 @@ export const ProductDetail = () =>{
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
                 <div>
-                    <XMarkIcon className='h-6 w-6 text-black hover:text-red-600' 
+                    <XMarkIcon className='h-6 w-6 text-black cursor-pointer hover:text-red-600' 
                     onClick={()=> context.closeDetail()}/>
                 </div>
             </div>
+                <figure className='px-6'>
+                    <img className='w-full h-full rounded-lg p-2'
+                    src={context.productToShow.images} 
+                    alt={context.productToShow.title}
+                    />
+                </figure>
+                <p className='flex flex-col p-6'>
+                    <span className='font-medium text-2xl mb-2'>${context.productToShow.price}</span>
+                    <span className='font-medium text-md'>{context.productToShow.title}</span>
+                    <span className='font-light text-sm'>{context.productToShow.description}</span>
+                </p>
         </aside>
     )
 }
--- vite-project/src/Context/index.jsx
+++ vite-project/src/Context/index.jsx
@@ -3,11 +3,18 @@ import { createContext, useState } from 'react';
 export const Store = createContext();
 
 export const StoreProvider = ({children})=>{
-    const [count,setCount] = useState(0);
-    const [productDetailOpen,setProductDetailOpen] = useState(false);
 
+    // shopping cart 
+    const [count , setCount] = useState(0);
+    
+    // product-detail open/close
+    const [productDetailOpen , setProductDetailOpen] = useState(false);
     const openDetail = ()=> setProductDetailOpen(true);
     const closeDetail = ()=> setProductDetailOpen(false);
+
+    //product-detail show product
+    const [productToShow , setProductToShow] = useState({});
+
 
     return(
         <Store.Provider value={{
@@ -15,7 +22,9 @@ export const StoreProvider = ({children})=>{
             setCount,
             openDetail,
             closeDetail,
-            productDetailOpen
+            productDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </Store.Provider>
