29d40d908b539d430350d78f4333b6f668563b5c
= <=>
2023-06-15 22:30:04 -0300

    estilizando lista de ordenes y detalles de ordenes

--- vite-project/src/Pages/MyOrders/index.jsx
+++ vite-project/src/Pages/MyOrders/index.jsx
@@ -8,8 +8,8 @@ function MyOrders() {
   const context = useContext(Store);
     return (
       <Layout>
-        <div className='flex justify-center items-center relative w-80'>
-          <h1>my orders</h1>
+        <div className='flex justify-center items-center relative w-80 mb-4'>
+          <h1 className='font-medium text-xl'>my orders</h1>
         </div>
         {
           context.order.map((order , index)=>(
